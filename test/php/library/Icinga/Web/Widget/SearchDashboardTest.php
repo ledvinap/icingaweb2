65f20057648d6ea42d83be4ff8a9f2204d6a0d19
Yonas Habteab <yonas.habteab@icinga.com>
2022-06-01 14:56:52 +0200

    SearchDashboardTest: Adjust unit tests according to the new dashboards

--- test/php/library/Icinga/Web/Widget/SearchDashboardTest.php
+++ test/php/library/Icinga/Web/Widget/SearchDashboardTest.php
@@ -3,79 +3,46 @@
 
 namespace Tests\Icinga\Web;
 
-use Icinga\Authentication\Role;
-use Mockery;
-use Icinga\Test\BaseTestCase;
-use Icinga\User;
+use Icinga\Exception\Http\HttpNotFoundException;
+use Icinga\Test\BaseDashboardTestCase;
 use Icinga\Web\Widget\SearchDashboard;
 
-class SearchDashboardTest extends BaseTestCase
+class SearchDashboardTest extends BaseDashboardTestCase
 {
-    public function setUp(): void
-    {
-        $moduleMock = Mockery::mock('Icinga\Application\Modules\Module');
-        $searchUrl = (object) array(
-            'title' => 'Hosts',
-            'url'   => 'monitoring/list/hosts?sort=host_severity&limit=10'
-        );
-        $moduleMock->shouldReceive('getSearchUrls')->andReturn(array(
-            $searchUrl
-        ));
-        $moduleMock->shouldReceive('getName')->andReturn('test');
-
-        $moduleManagerMock = Mockery::mock('Icinga\Application\Modules\Manager');
-        $moduleManagerMock->shouldReceive('getLoadedModules')->andReturn(array(
-            'test-module' => $moduleMock
-        ));
-
-        $bootstrapMock = $this->setupIcingaMock();
-        $bootstrapMock->shouldReceive('getModuleManager')->andReturn($moduleManagerMock);
-    }
-
     public function testWhetherRenderThrowsAnExceptionWhenHasNoDashlets()
     {
-        $this->expectException(\Zend_Controller_Action_Exception::class);
+        $this->expectException(HttpNotFoundException::class);
 
-        $user = new User('test');
-        $user->setPermissions(array('*' => '*'));
         $dashboard = new SearchDashboard();
-        $dashboard->setUser($user);
-        $dashboard = $dashboard->search('pending');
-        $dashboard->getPane('search')->removeDashlets();
+        $dashboard->setUser($this->getUser());
+        $dashboard->search('pending');
+
+        $searchHome = $dashboard->getActiveHome();
+        $searchHome->getEntry(SearchDashboard::SEARCH_PANE)->setEntries([]);
         $dashboard->render();
     }
 
     public function testWhetherSearchLoadsSearchDashletsFromModules()
     {
-        $role = new Role();
-        $role->setPermissions(['*']);
-
-        $user = new User('test');
-        $user->setRoles([$role]);
-
         $dashboard = new SearchDashboard();
-        $dashboard->setUser($user);
-        $dashboard = $dashboard->search('pending');
+        $dashboard->setUser($this->getUser());
+        $dashboard->search('pending');
 
-        $result = $dashboard->getPane('search')->hasDashlet('Hosts: pending');
+        $searchHome = $dashboard->getActiveHome();
+        $result = $searchHome->getEntry(SearchDashboard::SEARCH_PANE)->hasEntry('Hosts: pending');
 
-        $this->assertTrue($result, 'Dashboard::search() could not load search dashlets from modules');
+        $this->assertTrue($result, 'SearchDashboard::search() could not load search dashlets from modules');
     }
 
     public function testWhetherSearchProvidesHintWhenSearchStringIsEmpty()
     {
-        $role = new Role();
-        $role->setPermissions(['*']);
-
-        $user = new User('test');
-        $user->setRoles([$role]);
-
         $dashboard = new SearchDashboard();
-        $dashboard->setUser($user);
-        $dashboard = $dashboard->search();
+        $dashboard->setUser($this->getUser());
+        $dashboard->search();
 
-        $result = $dashboard->getPane('search')->hasDashlet('Ready to search');
+        $searchHome = $dashboard->getActiveHome();
+        $result = $searchHome->getEntry(SearchDashboard::SEARCH_PANE)->hasEntry('Ready to search');
 
-        $this->assertTrue($result, 'Dashboard::search() could not get hint for search');
+        $this->assertTrue($result, 'SearchDashboard::search() could not get hint for search');
     }
 }
